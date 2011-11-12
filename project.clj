0ca7d7feec5b6d44103576fd9e0d3769886b858e
Reid Draper <reiddraper@gmail.com>
2011-11-12 17:17:19 -0500

    Re-word description and add short readme

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject knockbox "0.0.1-SNAPSHOT"
-  :description "An eventually-consistent toolbox for Clojure, and eventually the JVM in general."
+  :description "An eventual-consistency toolbox for Clojure, and eventually the JVM in general."
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[lein-marginalia "0.6.0"]])
