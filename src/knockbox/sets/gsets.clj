a5d723198300d8b669835a29ba56a1b535b1e6fb
Reid Draper <reiddraper@gmail.com>
2011-11-12 20:13:15 -0500

    2-spaces instead of 4

--- src/knockbox/sets/gsets.clj
+++ src/knockbox/sets/gsets.clj
@@ -1,34 +1,34 @@
 (ns knockbox.sets.gsets
-    "This is an implementation of a
-    state-based grow-only
-    set data type. Operations are limited
-    to set-addition, and the merge algorithm
-    is simply the union of the two sets.")
+  "This is an implementation of a
+  state-based grow-only
+  set data type. Operations are limited
+  to set-addition, and the merge algorithm
+  is simply the union of the two sets.")
 
 (defn gset
-    "Create a new grow-only set,
-    optionally accepting a starting
-    set"
-    [& args]
-    (or (first args) #{}))
+  "Create a new grow-only set,
+  optionally accepting a starting
+  set"
+  [& args]
+  (or (first args) #{}))
 
 (defn add
-    "Add an item to a set"
-    [s item]
-    (conj s item))
+  "Add an item to a set"
+  [s item]
+  (conj s item))
 
 (defn merge
-    "Merge two sets together"
-    [a b]
-    (clojure.set/union a b))
+  "Merge two sets together"
+  [a b]
+  (clojure.set/union a b))
 
 (defn items
-    "Return all of the items in the set"
-    [s]
-    (seq s))
+  "Return all of the items in the set"
+  [s]
+  (seq s))
 
 (defn exists?
-    "Check for the existence of
-    a particular item in the set"
-    [s item]
-        (boolean (s item)))
+  "Check for the existence of
+  a particular item in the set"
+  [s item]
+  (boolean (s item)))
--- src/knockbox/sets/twopsets.clj
+++ src/knockbox/sets/twopsets.clj
@@ -1,49 +1,49 @@
 (ns knockbox.sets.twopsets
-    "This is an implementation of a
-    state-based
-    Two-Phase set data type.
-    This data type allows deletes,
-    with the limitation that items
-    can only be deleted that are in
-    the set (the local replica) and
-    that items can not be added back
-    once they've been deleted. The merge
-    function simply takes the set union
-    of each replica's `adds` and `deletes`.")
+  "This is an implementation of a
+  state-based
+  Two-Phase set data type.
+  This data type allows deletes,
+  with the limitation that items
+  can only be deleted that are in
+  the set (the local replica) and
+  that items can not be added back
+  once they've been deleted. The merge
+  function simply takes the set union
+  of each replica's `adds` and `deletes`.")
 
 (defstruct kb2pset :adds :dels)
 
 (defn twopset
-    "Create a new 2p-set,
-    optionally accepting a starting
-    set"
-    [& args]
-    (let [initial (or (first args) #{})]
-        (struct-map kb2pset
-                    :adds initial
-                    :dels #{})))
+  "Create a new 2p-set,
+  optionally accepting a starting
+  set"
+  [& args]
+  (let [initial (or (first args) #{})]
+    (struct-map kb2pset
+                :adds initial
+                :dels #{})))
 
 (defn add
-    "Add an item to a set"
-    [s item]
-    (assoc s :adds
-        (conj (:adds s) item)))
+  "Add an item to a set"
+  [s item]
+  (assoc s :adds
+    (conj (:adds s) item)))
 
 (defn remove
-    "Remove an `item` from the set `s`.
-    `item` must already be in the set."
-    [s item]
-    (assoc s :dels
-        (conj (s :dels) item)))
+  "Remove an `item` from the set `s`.
+  `item` must already be in the set."
+  [s item]
+  (assoc s :dels
+    (conj (s :dels) item)))
 
 (defn merge
-    "Merge two sets together"
-    [a b]
-    (let [adds (clojure.set/union
-                    (a :adds) (b :adds))
-          dels (clojure.set/union
-                    (a :dels) (b :dels))]
-        (struct-map kb2pset :adds adds :dels dels)))
+  "Merge two sets together"
+  [a b]
+  (let [adds (clojure.set/union
+                (a :adds) (b :adds))
+        dels (clojure.set/union
+                (a :dels) (b :dels))]
+    (struct-map kb2pset :adds adds :dels dels)))
 
 (defn exists?
     "Check for the existence
