331a540b03f9f3778443f5b2c744e248ce50c78e
Reid Draper <reiddraper@gmail.com>
2011-11-14 22:55:06 -0500

    Add protocol specification that should have been in the last commit

---
+++ src/knockbox/sets.clj
@@ -0,0 +1,30 @@
+;; -------------------------------------------------------------------
+;; Copyright (c) 2011 Basho Technologies, Inc.  All Rights Reserved.
+;;
+;; This file is provided to you under the Apache License,
+;; Version 2.0 (the "License"); you may not use this file
+;; except in compliance with the License.  You may obtain
+;; a copy of the License at
+;;
+;;   http://www.apache.org/licenses/LICENSE-2.0
+;;
+;; Unless required by applicable law or agreed to in writing,
+;; software distributed under the License is distributed on an
+;; "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+;; KIND, either express or implied.  See the License for the
+;; specific language governing permissions and limitations
+;; under the License.
+;;
+;; -------------------------------------------------------------------
+
+(ns knockbox.sets)
+
+(defprotocol kbset
+  "Basic set operations that a
+  convergent set must implement"
+  (add      [s item]      "Add `item` to `s`")
+  (delete   [s item]      "Delete `item` from `s`")
+  (resolve  [a b & more]  "Resolve conflicts between `a`, `b`
+                          and `more` and return the resolved object"))
+  (exists?  [s item]      "Check for the existence of `item` in `s`")
+  (items    [s]           "Return all of the items in `s`")
